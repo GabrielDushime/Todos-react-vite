caa1c7c35d2e22fb70594a128a45de10bdafc932
Gabriel DUSHIME <108078955+GabrielDushime@users.noreply.github.com>
2024-04-30 10:53:21 +0200

    My todos using react

---
+++ src/Data.jsx
@@ -0,0 +1,23 @@
+export const userList=[
+
+
+
+
+    {
+        name:"Gabriel DUSHIME",
+        email:"dushimegabriel@gmail.com",
+        id:1
+    },
+    {
+        name:"Armel MUNYANEZA",
+        email:"munyarmel@gmail.com",
+        id:2
+    },
+    {
+        name:"Ismael MUREKEZI",
+        email:"ismamurekezi@gmail.com",
+        id:3
+    }
+    
+
+]
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import UserReducer from './UserReducer'
+
+
+const store=configureStore({
+    reducer:{
+         users:UserReducer
+    }
+})
+
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+
+        <App/> 
+
+        </Provider>
+        
+    </React.StrictMode>
+)
